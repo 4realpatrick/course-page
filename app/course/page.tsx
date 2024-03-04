d0d410d6ee23362a2fd3720be66a0fbbef9dd401
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-04 09:55:51 +0800

    feat: add animation

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -12,6 +12,8 @@ import {
   Suspense,
 } from "react";
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+import { LazyMotion, m, domAnimation } from "framer-motion";
+
 const Switcher = ({
   orientation,
   onClick,
@@ -20,11 +22,14 @@ const Switcher = ({
   onClick: React.MouseEventHandler<HTMLDivElement>;
 }) => {
   return (
-    <div
+    <m.div
       className={cn(
         "absolute h-full w-10 z-10 flex items-center justify-center top-0 mx-2",
         orientation === "right" && "right-0"
       )}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
     >
       <div
         className="p-4 rounded-full border border-gray-500 inline-flex items-center justify-center bg-neutral-950 hover:bg-gray-500 cursor-pointer transition-[background]"
@@ -36,9 +41,10 @@ const Switcher = ({
           <MdArrowForwardIos className="size-4 text-white" />
         )}
       </div>
-    </div>
+    </m.div>
   );
 };
+
 const CoursePage = () => {
   const [mounted, setMounted] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -68,33 +74,38 @@ const CoursePage = () => {
   if (!mounted) return null;
 
   return (
-    <div className="h-[inherit] flex items-center justify-center overflow-hidden">
-      <div className="w-full overflow-hidden relative lg:min-w-[1000px] lg:w-4/5">
-        {canSwitch("left") && (
-          <Switcher
-            orientation="left"
-            onClick={() => setCurrentIndex(currentIndex - 1)}
-          />
-        )}
-        <div
-          className={cn(
-            "relative flex gap-[5%] px-8 py-4 transition-transform"
+    <LazyMotion features={domAnimation}>
+      <div className="h-[inherit] flex items-center justify-center overflow-hidden">
+        <div className="w-full overflow-hidden relative lg:min-w-[1000px] lg:w-4/5">
+          {canSwitch("left") && (
+            <Switcher
+              orientation="left"
+              onClick={() => setCurrentIndex(currentIndex - 1)}
+            />
           )}
-          ref={containerRef}
-          style={{
-            transform: `translateX(-${currentIndex * (isMobile ? 95 : 35)}%)`,
-          }}
-        >
-          {cards}
+          <m.div
+            className={cn(
+              "relative flex gap-[5%] px-8 py-4 transition-transform"
+            )}
+            ref={containerRef}
+            style={{
+              transform: `translateX(-${currentIndex * (isMobile ? 95 : 35)}%)`,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+          >
+            {cards}
+          </m.div>
+          {canSwitch("right") && (
+            <Switcher
+              orientation="right"
+              onClick={() => setCurrentIndex(currentIndex + 1)}
+            />
+          )}
         </div>
-        {canSwitch("right") && (
-          <Switcher
-            orientation="right"
-            onClick={() => setCurrentIndex(currentIndex + 1)}
-          />
-        )}
       </div>
-    </div>
+    </LazyMotion>
   );
 };
 
